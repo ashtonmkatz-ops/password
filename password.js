ff5301b63b5f5c400fd6b3d1e440e9f19c8d9ad4
gigglyfmap <ashtonmkatz@gmail.com>
2025-12-05 15:36:48 -0700

    Update password.js

--- password.js
+++ password.js
@@ -1,1 +1,14 @@
-var password = "vuulinox"
+
+function checkPassword() {
+            var password = "vuulinox"
+            var userInput = document.getElementById("pswd").value;
+    
+            if (userInput === password) {
+                // Password is correct, show the content
+                document.getElementById("protectedContent").style.display = "block";
+            } else {
+                // Password is incorrect, redirect or show an error
+                alert("Incorrect password. You will be redirected.");
+               
+            }
+        }
